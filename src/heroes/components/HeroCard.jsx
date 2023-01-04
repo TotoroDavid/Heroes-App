c9bb8e423c6584a6e5451374e04c23428232d50d
DavidToro <davidtoro65@hotmail.com>
2023-01-04 15:16:34 -0500

    my second commit

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,24 +1,24 @@
 import { Link } from 'react-router-dom';
 
-const CharactersByHero = ({ alter_ego, characters}) => {
+const CharactersByHero = ({ alter_ego, characters }) => {
     // if ( alter_ego === characters ) return (<></>);
     // return <p>{ characters }</p>
-    return ( alter_ego === characters )
-     ? <></>
-     : <p>{ characters }</p>;
+    return (alter_ego === characters)
+        ? <></>
+        : <p>{characters}</p>;
 }
 
 
-export const HeroCard = ({ 
+export const HeroCard = ({
     id,
     superhero,
     publisher,
     alter_ego,
     first_appearance,
-    characters ,
+    characters,
 }) => {
 
-    const heroImageUrl = `/assets/heroes/${ id }.jpg`;
+    const heroImageUrl = `/assets/heroes/${id}.jpg`;
 
     // const charactesByHero =  (<p>{ characters }</p>);
 
@@ -28,33 +28,33 @@ export const HeroCard = ({
             <div className="card">
 
                 <div className="row no-gutters">
-                    
+
                     <div className="col-4">
-                        <img src={ heroImageUrl } className="card-img" alt={ superhero } />
+                        <img src={heroImageUrl} className="card-img" alt={superhero} />
                     </div>
 
                     <div className="col-8">
 
                         <div className="card-body">
 
-                            <h5 className="card-title">{ superhero }</h5>
-                            <p className="card-text">{ alter_ego }</p>
+                            <h5 className="card-title">{superhero}</h5>
+                            <p className="card-text">{alter_ego}</p>
 
                             {/* {
                                 ( alter_ego !== characters ) && charactesByHero
                                 ( alter_ego !== characters ) && <p>{ characters }</p>
                             } */}
-                            <CharactersByHero characters={ characters } alter_ego={ alter_ego } />
+                            <CharactersByHero characters={characters} alter_ego={alter_ego} />
 
                             <p className="card-text">
-                                <small className="text-muted">{ first_appearance }</small>
+                                <small className="text-muted">{first_appearance}</small>
                             </p>
 
-                            <Link to={`/hero/${ id }`}>
-                                Más..
+                            <Link to={`/hero/${id}`}>
+                                More . . .
                             </Link>
 
-                            
+
                         </div>
 
                     </div>
